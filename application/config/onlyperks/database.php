dba67a3018a6c29806ddaceaeb3c53426d0a8b93
Elias Espino <elias@collaborativeperks.com>
2020-01-23 15:55:18 +0100

    doonamis conection

--- application/config/onlyperks/database.php
+++ application/config/onlyperks/database.php
@@ -73,7 +73,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+
 $db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => '54.78.135.183',
+	'username' => 'HFSDoonamis',
+	'password' => 'HFSDoonamisBelt',
+	'database' => 'HFSDoonamis',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+/*$db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'elias',
@@ -93,4 +116,4 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
-);
+);*/
